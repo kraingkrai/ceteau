c9ad8b39c90ece9f8aada8e972448c5e0fb41431
Benz Kraingkrai <kraingkrai.benz@gmail.com>
2017-10-17 11:44:56 +0700

    Re-design UI by Jae.

---
+++ js/global.js
@@ -0,0 +1,25 @@
+(function ($) {
+    //Variables Start
+    //Variables End
+
+    //Page Start
+    initHomeSlider();
+    //Page Load
+
+    //Event Start
+    //Event End
+
+    //Function Start
+    function initHomeSlider() {
+        $('#homeTestimonialContent').lightSlider({
+            item: 1,
+            slideMargin: 0,
+            speed: 1000,
+            auto: true,
+            loop: true,
+            pause: 5000,
+            mode: 'fade'
+        });
+    }
+    //Function End
+})($);
