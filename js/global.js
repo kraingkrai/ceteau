ea95b0b55c29d9b3e237b7c0985b476bddba4483
Benz Kraingkrai <kraingkrai.benz@gmail.com>
2017-12-07 21:04:54 +0700

    Update UI.

--- js/global.js
+++ js/global.js
@@ -7,6 +7,22 @@
     //Page Load
 
     //Event Start
+    $('html').click(function () {
+        closeMenu();
+    });
+    
+    $('#closeMenuBtn').click(function () {
+        closeMenu();
+    });
+    
+    $('#openMenuBtn').click(function (event) {
+        $('#menuContainer').css('right', '0');
+        event.stopPropagation();
+    });
+
+    $('#menuContainer').click(function (event) {
+        event.stopPropagation();
+    });
     //Event End
 
     //Function Start
@@ -39,14 +55,18 @@
             slideMargin: 50,
             pager: false
         });
-        
+
         $('#newsSliderPrev').on('click', function () {
-            newsSlider.goToPrevSlide();      
+            newsSlider.goToPrevSlide();
         });
 
         $('#newsSliderNext').on('click', function () {
             newsSlider.goToNextSlide();
         });
+    }
+
+    function closeMenu() {
+        $('#menuContainer').css('right', '-400px');
     }
     //Function End
 })($);
