5774ad3a97f65f0110d32a022554cd73721abf5e
Benz Kraingkrai <kraingkrai.benz@gmail.com>
2018-04-10 11:17:52 +0700

    Update UI.

---
+++ js/project-detail.js
@@ -0,0 +1,23 @@
+(function ($) {
+    //Variables Start
+    //Variables End
+
+    //Page Start
+    $('#projectSlider').lightSlider({
+        item: 1,
+        slideMargin: 0,
+        speed: 1000,
+        auto: true,
+        loop: true,
+        pause: 5000,
+        mode: 'fade',
+        pager: false
+    });
+    //Page Load
+
+    //Event Start
+    //Event End
+
+    //Function Start
+    //Function End
+})($);
