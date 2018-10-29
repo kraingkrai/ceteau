e6c35d436f60fbfa7fe61beb111cd29b0091bac3
Benz Kraingkrai <kraingkrai.benz@gmail.com>
2018-10-29 22:18:10 +0700

    Add calculator page.

---
+++ js/calculator.js
@@ -0,0 +1,17 @@
+(function ($) {
+    //Variables Start
+    //Variables End
+
+    //Page Start
+    //Page Load
+
+    //Event Start
+    $('#calculateBtn').click(function() {
+        $('#points').val(10);
+        $('#quantity').val(20);
+    });
+    //Event End
+
+    //Function Start
+    //Function End
+})($);
